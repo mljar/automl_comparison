5a7e9062ba4db1f5e37fbeb2c21f23bf6d8dfd25
pp <pplonski86@gmail.com>
2017-12-07 13:39:45 +0100

    data download script

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # automl_comparison
-Comparison of automatic machine learning libraries
+This is code for comparison of automatic machine learning libraries:
+
+ - auto-sklearn
+ - autoML from h2o
+ - mljar
+
+### Datasets used for comparison
+
+| Dataset Id | Name | Rows | Columns |
+| - | - | - | - |
+| 3 | kr-vs-kp | 3196 | 36 |
+| 24 | mushroom | 8124 | 22 |
+| 31 | credit-g | 1000 | 20 |
+| 38 | sick | 3772 | 29 |
+| 44 | spambase | 4601 | 57 |
+| 179 | adult | 48842 | 14 |
+| 715 | fri_c3_1000_25 | 1000 | 25 |
+| 718 | fri_c4_1000_100 | 1000 | 100 |
+| 720 | abalone | 4177 | 8 |
+| 722 | pol | 15000 | 48 |
+| 723 | fri_c4_1000_25 | 1000 | 25 |
+| 727 | 2dplanes | 40768 | 10 |
+| 728 | analcatdata_supreme | 4052 | 7 |
+| 734 | ailerons | 13750 | 40 |
+| 735 | cpu_small | 8192 | 12 |
+| 737 | space_ga | 3107 | 6 |
+| 740 | fri_c3_1000_10 | 1000 | 10 |
+| 741 | rmftsa_sleepdata | 1024 | 2 |
+| 819 | delta_elevators | 9517 | 6 |
+| 821 | house_16H | 22784 | 16 |
+| 822 | cal_housing | 20640 | 8 |
+| 823 | houses | 20640 | 8 |
+| 833 | bank32nh | 8192 | 32 |
+| 837 | fri_c1_1000_50 | 1000 | 50 |
+| 843 | house_8L | 22784 | 8 |
+| 845 | fri_c0_1000_10 | 1000 | 10 |
+| 846 | elevators | 16599 | 18 |
+| 847 | wind | 6574 | 14 |
+
+### Methodology
+
+
+### Results
---
+++ get_data.py
@@ -0,0 +1,37 @@
+__author__ = 'Piotr Plonski'
+
+import os
+import openml
+import pandas as pd
+import numpy as np
+
+openml.config.apikey = os.environ['OPENML_KEY']
+
+dataset_ids = [3, 24, 31, 38, 44, 179, 715, 718, 720, 722, 723, 727, 728, 734, 735, 737, 740, 741,
+               819, 821, 822, 823, 833, 837, 843, 845, 846, 847]
+
+
+for dataset_id in dataset_ids:
+    print 'Get dataset id', dataset_id
+    dataset = openml.datasets.get_dataset(dataset_id)
+    (X, y, categorical, names) = dataset.get_data(target=dataset.default_target_attribute, \
+                                                    return_categorical_indicator=True, \
+                                                    return_attribute_names=True)
+    if len(np.unique(y)) != 2:
+        print 'Not binary classification'
+        continue
+    vals = {}
+    for i, name in enumerate(names):
+        vals[name] = X[:,i]
+    vals['target'] = y
+    df = pd.DataFrame(vals)
+    df.to_csv('./data/{0}.csv'.format(dataset_id), index=False)
+
+
+# Print table with datasets descriptions for Readme
+#print '| Dataset Id | Name | Rows | Columns |'
+#print '| - | - | - | - |'
+#for dataset_id in dataset_ids:
+#    dataset = openml.datasets.get_dataset(dataset_id)
+#    (X, y, categorical, names) = dataset.get_data(target=dataset.default_target_attribute,return_categorical_indicator=True, return_attribute_names=True)
+#    print '| {0} | {1} | {2} | {3} |'.format(dataset_id, dataset.name, X.shape[0], X.shape[1])
